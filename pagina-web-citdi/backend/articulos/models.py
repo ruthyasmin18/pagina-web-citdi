65a858aef4914c110abf41feddb97bdca92d88f3
Ruth Amaya <rhuacache005@gmail.com>
2025-04-15 15:45:26 -0500

    Primer commit

---
+++ pagina-web-citdi/backend/articulos/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Articulo(models.Model):
+    titulo = models.CharField(max_length=100)
+    contenido = models.TextField()
+    fecha_publicacion = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titulo
